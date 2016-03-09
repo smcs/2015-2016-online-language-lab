de222cd5fa868e514787491746be9bd85c45058c
Seth Battis <sethbattis@stmarksschool.org>
2016-03-09 13:49:36 -0500

    Test webhook deploy

--- Templates Example/index.php
+++ Templates Example/index.php
@@ -7,7 +7,7 @@ $smarty->addTemplateDir(__DIR__ . '/templates');
 
 // ...do some stuff...
 
-$smarty->assign('title', 'Whoopti-doo!');
+$smarty->assign('title', 'It works!');
 $smarty->assign('paragraph', 'The quick brown fox jumps over the lazy dog.');
 $smarty->assign('list', array(1, 1, 2, 3, 5, 8, 13, 21));
 $smarty->display('example.tpl');
