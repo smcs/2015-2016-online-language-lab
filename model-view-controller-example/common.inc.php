db68fd3a786d202d65a343b908f6deafcd42372d
Seth Battis <sethbattis@stmarksschool.org>
2016-04-01 09:40:46 -0400

    fix

--- model-view-controller-example/common.inc.php
+++ model-view-controller-example/common.inc.php
@@ -1,6 +1,6 @@
 <?php
 	
-require_once __DIR__ . '/../Language\ Lab\ v1/vendor/autoload.php';
+require_once __DIR__ . '/../Language Lab v1/vendor/autoload.php';
 require_once __DIR__ . '/../database-example/secrets.inc.php';
 
 $smarty = Battis\BootstrapSmarty\BootstrapSmarty::getSmarty();
