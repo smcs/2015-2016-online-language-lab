680fd754bf64e39d86d7bc0ed6f1fb705e3b2bf2
aigerim <aigerimbishigayeva@stmarksschool.org>
2016-05-02 17:11:30 -0400

    Working on modal

--- teacherclass.php
+++ teacherclass.php
@@ -8,7 +8,7 @@
       <ul class="nav nav-sidebar">
         <li><a href="#">Student 1</a></li>
         <li><a href="#">Student 2</a></li>
-        <li><a href="#">student 3</a></li>
+        <li><a href="#">Student 3</a></li>
         <li><a href="#">Student 4</a></li>
       </ul>
       <ul class="nav nav-sidebar">
@@ -40,23 +40,38 @@
     </div>
 
     <!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="myModalLabel">New Assignment</h4>
+          </div>
+          <div class="modal-body">
+            <div class="input-group">
+              <span class="input-group-addon" id="basic-addon1">Assignment Title</span>
+              <input type="text" class="form-control" placeholder="Title" aria-describedby="basic-addon1">
+            </div>
+            <div class="input-group">
+              <span class="input-group-addon" id="basic-addon1">Students</span>
+              <input type="text" class="form-control" placeholder="Drag and drop" aria-describedby="basic-addon1">
+            </div>
+            <div class="input-group">
+              <span class="input-group-addon" id="basic-addon1">Instructions</span>
+              <input type="text" class="form-control" placeholder="Istructions" aria-describedby="basic-addon1">
+            </div>
+            <div class="input-group">
+              <span class="input-group-addon" id="basic-addon1">Upload media</span>
+              <input type="text" class="form-control" placeholder="Upload" aria-describedby="basic-addon1">
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
 
   </div>
 </div>
