2d318f15cba22affaa969a819a088d673c7c83f2
Seth Battis <sethbattis@stmarksschool.org>
2016-05-04 09:58:01 -0400

    var_dump sessiond ata

--- StudentDashboard.php
+++ StudentDashboard.php
@@ -1,4 +1,9 @@
 <?php
 require_once 'common.inc.php';
+
+session_start();
+
+var_dump($_SESSION['consumer']);
+
 /* do lots of other prep work before displaying */
 $smarty->display('StudentDashboard.tpl');
