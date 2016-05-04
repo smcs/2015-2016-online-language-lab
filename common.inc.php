8da466b4327c063839b56c5af1458654b27cba81
Seth Battis <sethbattis@stmarksschool.org>
2016-05-03 20:56:52 -0400

    LTI-ification
    First pass at configuring an LTI. (The config.xml file is from the Edu App Center generator -- yay for convenience).

--- common.inc.php
+++ common.inc.php
@@ -2,6 +2,10 @@
 
 require_once 'vendor/autoload.php';
 
+$secrets = new Battis\ConfigXML(__DIR__ . '/secrets.xml');
+
+$sql = $secrets->newInstanceOf(mysqli::class, '//mysql');
+
 $smarty = Battis\BootstrapSmarty\BootstrapSmarty::getSmarty();
 $smarty->addTemplateDir(__DIR__ . '/templates');
 
---
+++ src/LanguageLabLTI.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace smcs\language_lab;
+
+class LanguageLabLTI extends LTI_Tool_Provider {
+
+    /* called when the LTI is launched */
+    public function onLaunch() {
+        /* store the user/course information from Canvas (the tool consumer) in
+           the $_SESSION global */
+        $_SESSION['consumer'] = $this->user->getResourceLink()->settings;
+
+        /* decide which dashboard to load based on user role */
+        if ($this->user->isLearner()) {
+            $this->redirectURL = 'StudentDashboard.php';
+        } elseif ($this->user->isStaff() || $this->user->isAdmin()) {
+            $this->redirectURL = 'teacherdash.php';
+        } else {
+            $this->reason = 'Invalid role';
+            $this->isOK = false;
+        }
+    }
+}
