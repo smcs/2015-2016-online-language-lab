0f0778b1bdff92bc8f47fa39cc81873ed034283a
aigerim <aigerimbishigayeva@stmarksschool.org>
2016-05-09 11:52:46 -0400

    deleted test

--- tests/OpenTok/api/common.inc.php
+++
@@ -1,3 +0,0 @@
-<?php
-	
-require_once __DIR__ . '/../common.inc.php';
--- tests/OpenTok/session.php
+++
@@ -1,6 +0,0 @@
-<?php
-	
-require_once 'common.inc.php';
-$smarty->addStylesheet('css/session.css.php');
-$smarty->assign('id', (empty($_REQUEST['session']) ? '' : $_REQUEST['session']));
-$smarty->display('session.tpl');
