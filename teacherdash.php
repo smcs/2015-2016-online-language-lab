e88947afb19aeaf002484d6931ece1d2d447f66c
Seth Battis <sethbattis@stmarksschool.org>
2016-05-09 12:06:10 -0400

    Empty id

--- teacherdash.php
+++ teacherdash.php
@@ -5,4 +5,5 @@ require_once 'common.inc.php';
 /* do lots of other prep work before displaying */
 
 
+$smarty->assign('id', (empty($_REQUEST['session']) ? '' : $_REQUEST['session']));
 $smarty->display('teacherdash.tpl');
