4bf2e10e004f48b291c7012c49bb411ed9b00a37
Seth Battis <sethbattis@stmarksschool.org>
2016-05-13 09:51:14 -0400

    SESSION API endpoint

--- api/session.php
+++ api/session.php
@@ -13,62 +13,56 @@ https://roswell.stmarksschool.org/~language-lab/api/session.php?type=main&class=
 */
 
 /* what type of session to create? ($_REQUEST['type']) */
+$type = trim(
+	strtoupper(
+		(empty($_REQUEST['type']) ? '' : $_REQUEST['type'])
+	)
+);
+switch($type) {
+	case 'GROUP':
+		$type = 'GROUP';
+		break;
+	case 'MAIN':
+	default:
+		$type = 'MAIN';
+		break;
+}
+
 /* create that type of session */
+// TODO we probably need to set some configuration options
+$session = $opentok->createSession();
+
 /* store session data in db...
 	if main session, it needs to be associated with this class ($_REQUEST['class']), store into sessions table with type 'main', replacing any other main sessions for this class (and disable/delete old group sessions for this class)
 	if group session, it needs to be associated with this class ($_REQUEST['class']), store into sessions table with type 'group'
 */
-/* put together AJAX response with apiKey, sessionId, token parameters */
-/* send AJAX response */
-
-if (empty($_REQUEST['id'])) {
-	$session = $opentok->createSession();
-	$result = $sql->query("
-		INSERT INTO `sessions`
-			(
-				`creator`,
-				`tokbox`
-			)
-			VALUES (
-				'-1',
-				'" . $sql->escape_string($session->getSessionId()) . "'
-			)
-	");
-	if ($result == false) {
-		$smarty->addMessage(
-			"Database error {$sql->errno}",
-			$sql->error,
-			NotificationMessage::ERROR
-		);
-		$smarty->display('api/error.tpl');
-	}
-	$apiResponse['apiKey'] = $secrets->toArray('//opentok/key')[0][0];
-	$apiResponse['sessionId'] = $session->getSessionId();
-	$apiResponse['token'] = $opentok->generateToken($session->getSessionId());
-	$apiResponse['id'] = $sql->insert_id;
-} else {
-	$response = $sql->query("
-		SELECT *
-			FROM `sessions`
-			WHERE
-				`id` = '" . $sql->escape_string($_REQUEST['id']) . "'
-	");
-	if ($response) {
-		$session = $response->fetch_assoc();
-		$apiResponse['apiKey'] = $secrets->toArray('//opentok/key')[0][0];
-		$apiResponse['sessionId'] = $session['tokbox'];
-		$apiResponse['token'] = $opentok->generateToken($session['tokbox']);
-		$apiResponse['id'] = $session['id'];
-	} else {
-		$smarty->addMessage(
-			"Database error {$sql->errno}",
-			$sql->error,
-			NotificationMessage::ERROR
-		);
-		$smarty->display('api/error.tpl');
-	}
+if ($sql->query("
+	INSERT INTO `sessions`
+		(
+			`tokbox`,
+			`type`
+		) VALUES (
+			'" . $sql->escape_string($session->getSessionId()) . "',
+			'" . $type . "'
+		)
+") === false) {
+	$smarty->addMessage(
+		"Database error {$sql->errno}",
+		$sql->error,
+		NotificationMessage::ERROR
+	);
+	$smarty->display('api/error.tpl');
+	exit;
 }
 
+/* put together AJAX response with apiKey, sessionId, token parameters */
+$apiResponse['apiKey'] = $secrets->toString('//opentok/key');
+$apiResponse['sessionId'] = $session->getSessionId();
+$apiResponse['token'] = $opentok->generateToken($session->getSessionId());
+$apiResponse['type'] = $type;
+$apiResponse['id'] = $sql->insert_id;
+
+/* send AJAX response */
 header("Access-Control-Allow-Origin: {$_SERVER['SERVER_NAME']}");
 echo json_encode($apiResponse);
 exit;
