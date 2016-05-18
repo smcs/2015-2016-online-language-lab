138b7a5d6962653426d709d200274d22738cd366
Seth Battis <sethbattis@stmarksschool.org>
2016-05-18 10:00:36 -0400

    Launch without Smarty

--- common.inc.php
+++ common.inc.php
@@ -12,25 +12,26 @@ $opentok = $secrets->newInstanceOf(OpenTok::class, '//opentok');
 
 $sql = $secrets->newInstanceOf(mysqli::class, '//mysql');
 
-$smarty = Battis\BootstrapSmarty\BootstrapSmarty::getSmarty();
-$smarty->assign(
-    'rootURL',
-    (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on' ?
-        'http://' :
-        'https://'
-    ) .
-    $_SERVER['SERVER_NAME'] .
-    $_SERVER['CONTEXT_PREFIX'] .
-    str_replace(
-        $_SERVER['CONTEXT_DOCUMENT_ROOT'],
-        '',
-        __DIR__
-    )
-);
-$smarty->addTemplateDir(__DIR__ . '/templates');
-$smarty->assign('context', $_SESSION['user']->getResourceLink()->lti_context_id);
-$smarty->assign('user', $_SESSION['user']->getId());
-$smarty->assign('firstName', $_SESSION['user']->firstname);
-$smarty->assign('lastName', $_SESSION['user']->lastname);
-
+if (!(str_replace($_SERVER['CONTEXT_PREFIX'] . str_replace($_SERVER['CONTEXT_DOCUMENT_ROOT'], '', __DIR__), '', $_SERVER['REQUEST_URI']) == '/lti/launch.php')) {
+    $smarty = Battis\BootstrapSmarty\BootstrapSmarty::getSmarty();
+    $smarty->assign(
+        'rootURL',
+        (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on' ?
+            'http://' :
+            'https://'
+        ) .
+        $_SERVER['SERVER_NAME'] .
+        $_SERVER['CONTEXT_PREFIX'] .
+        str_replace(
+            $_SERVER['CONTEXT_DOCUMENT_ROOT'],
+            '',
+            __DIR__
+        )
+    );
+    $smarty->addTemplateDir(__DIR__ . '/templates');
+    $smarty->assign('context', $_SESSION['user']->getResourceLink()->lti_context_id);
+    $smarty->assign('user', $_SESSION['user']->getId());
+    $smarty->assign('firstName', $_SESSION['user']->firstname);
+    $smarty->assign('lastName', $_SESSION['user']->lastname);
+}
 ?>
