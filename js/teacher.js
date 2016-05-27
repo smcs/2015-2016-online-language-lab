79b3755e2f412e4ef22941ae539d3b6fbe992e5a
Seth Battis <sethbattis@stmarksschool.org>
2016-05-27 09:47:57 -0400

    Capture correct group ID

--- js/teacher.js
+++ js/teacher.js
@@ -49,7 +49,7 @@ Teacher.displayGroup = function(id) {
 }
 
 Teacher.sortableUpdate = function(event, ui) {
-    var groupID = $(ui.item[0]).parent().attr('id');
+    var groupID = this.parent().attr('id');
     var user = $(ui.item[0]).find('.embed-responsive-item').attr('user');
 
     /*
