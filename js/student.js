77e7c3da9b0cab6b4b0e951c5c658bb50ddee015
Seth Battis <sethbattis@stmarksschool.org>
2016-05-28 09:40:55 -0400

    hrm

--- js/student.js
+++ js/student.js
@@ -22,7 +22,6 @@ Student.initializeSession = function(apiKey, sessionId, token) {
     "use strict";
     this.__proto__.initializeSession(apiKey, sessionId, token);
     /* FIXME I think this works okay, but I'm worried that it may rely on too many assumptions */
-    console.log(this);
     this.sessions[(this.sessions.length > 0 ? this.sessions.length - 1 : 'ot-streams')].on('sessionDisconeected', function(event) {
         console.log('Disconnected. Cleaning up and rejoining.');
         $('#' + Student.thumbnailContainerID).empty();
@@ -31,6 +30,7 @@ Student.initializeSession = function(apiKey, sessionId, token) {
         Student.__proto__.streams = [];
         Student.joinSession();
     });
+    console.log(this);
 };
 
 Student.makeConnection = function() {
