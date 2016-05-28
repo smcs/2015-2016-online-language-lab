a10cf4b0b88e940bd66260a4367214961f3b7b66
Seth Battis <sethbattis@stmarksschool.org>
2016-05-28 10:21:18 -0400

    StreamEvent?

--- js/student.js
+++ js/student.js
@@ -22,6 +22,9 @@ Student.initializeSession = function(apiKey, sessionId, token) {
     "use strict";
     this.__proto__.initializeSession(apiKey, sessionId, token);
     /* FIXME I think this works okay, but I'm worried that it may rely on too many assumptions */
+    this.sessions[(this.sessions.length > 0 ? this.sessions.length - 1 : 'ot-streams')].on('streamEvent', function(event) {
+        console.log('Stream event!', event);
+    };
     this.sessions[(this.sessions.length > 0 ? this.sessions.length - 1 : 'ot-streams')].on('sessionDisconeected', function(event) {
         console.log('Disconnected (' + event.reason + '). Cleaning up and rejoining.');
         $('#' + Student.thumbnailContainerID).empty();
